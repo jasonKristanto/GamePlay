078b8741e2ac8f820f8e2d8a88b002988b9fe052
jansen18 <42839170+jansen18@users.noreply.github.com>
2019-05-20 18:35:18 +0700

    Update Home_View.php

--- application/views/pages/Home_View.php
+++ application/views/pages/Home_View.php
@@ -127,4 +127,44 @@
 		}
 
 	</style>
+	<script>
+		<?php if(isset($_GET['success'])) {?>
+
+		$.notify({
+			'...' : '...'
+		},{
+			element: 'body',
+			position: null,
+			type: "success",
+			allow_dismiss: true,
+			newest_on_top: false,
+			showProgressbar: false,
+			placement: {
+				from: "top",
+				align: "center"
+			},
+			offset: 20,
+			spacing: 10,
+			z_index: 1031,
+			delay: 5000,
+			timer: 1000,
+			url_target: '_blank',
+			mouse_over: null,
+			animate: {
+				enter: 'animated fadeInDown',
+				exit: 'animated fadeOutUp'
+			},
+			template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+				'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+				'<span data-notify="icon"></span> ' +
+				'<span data-notify="title"><strong>Purchase Success.</strong></span> </br>' +
+				'<span data-notify="message">Thank you for your puchases, enjoy your game.</span>' +
+				'<div class="progress" data-notify="progressbar">' +
+					'<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+				'</div>' +
+				'<a href="{3}" target="{4}" data-notify="url"></a>' +
+			'</div>'
+		});
+		<?php }?>
+	</script>
 </html>
