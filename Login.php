60146d6825d25b0b9c96b528d4ae86d2c1baf308
jansen18 <42839170+jansen18@users.noreply.github.com>
2019-05-20 16:36:31 +0700

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('Login_Model');
+	}
+
+	public function index()
+	{
+		if(isset($this->session->nama)){
+			redirect(base_url());
+		}
+
+		$data['js'] = $this->load->view('include/javascript.php', NULL, TRUE);
+		$data['css'] = $this->load->view('include/css.php', NULL, TRUE);
+		$data['header'] = $this->load->view('pages/header.php', NULL, TRUE);
+		$data['footer'] = $this->load->view('pages/footer.php', NULL, TRUE);
+
+		$this->load->view('pages/Login_View.php', $data);
+	}
+
+  public function login(){
+    $login_user = $this->Login_Model->get_user($_POST['login_username'], $_POST['login_password']);
+		if(sizeof($login_user) == 0){
+			redirect(base_url() . "Login?s=0");
+		}
+		else if(sizeof($login_user) > 0){
+			$user = array(
+				'nama'  => $login_user[0]['nama'] ,
+				'username' => $login_user[0]['username'],
+				'email' => $login_user[0]['email'],
+				'login' => 'sukses'
+			);
+			$this->session->set_userdata($user);
+			$_POST = NULL;
+			$_GET= NULL;
+			redirect(base_url());
+		}
+  }
+
+	public function logout(){
+		$this->session->unset_userdata('nama', 'username', 'email', 'login');
+
+		redirect(base_url());
+	}
+}
---
+++ Login_View.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+			echo $css;
+			echo $js;
+		?>
+    <title>Login GamePlay</title>
+	</head>
+	<body class="bgGrey2">
+		<?php echo $header; ?>
+		<div class="container-fluid wrapper">
+			<div class="card" style="width:300px;">
+				<div class="card-header">
+					<h4 class="modal-title text-center"><span style="color:black;">Game<strong style="color:blue;">Play</strong> Sign In</span></h4>
+				</div>
+				<form class="form-signin" method="post" action="Login/login">
+					<div class="form-group">
+						<input type="text" name="login_username" class="form-control" placeholder="Username" required>
+					</div>
+					<div class="form-group">
+                        <input type="password" name='login_password' class="form-control" placeholder="Password" required>
+                    </div>
+					<?php if(isset($_GET['s'])){ ?>
+						<div class="alert alert-warning" align="center">Login failed, please check your username or password</div> 
+					<?php }?>
+					<input class="btn btn-lg btn-primary btn-block" type="submit" name="Login" value="Login">
+				</form>
+				<div class="card-footer text-center">
+					<p class="text-center">Don't have any account? <a href="<?php echo base_url() . 'SignUp'; ?>">Sign Up</a></p>
+				</div>
+			</div>
+		</div>
+	</body>
+	<style>
+		.wrapper{
+			min-height: 75%; 
+			min-height: 75vh; 
+			display: flex;
+			align-items: center;
+		}
+		.card {
+			float: auto;
+			margin: 0 auto;
+			background: #EEEEEE;
+			box-shadow: 0 2px 2px rgba(0, 0, 0, 0.4);
+			border-radius: 5px;
+		} 
+		.card > form {
+			margin:10px;
+		} 
+		.card > form input[type=text], 
+		.card > form input[type=password] {
+			height: 40px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+		}
+		.card-header{
+			background-color: #F9F9F9;
+			border-bottom: 2px solid #999999;
+			padding: 15px;
+			text-align: center;
+			-webkit-border-radius: 10px 10px  0 0 ;
+		}
+		.card-footer{
+			background-color: #F9F9F9;
+			border-top: 2px solid #999999;
+			padding: 15px;
+			text-align: center;
+			-webkit-border-radius: 0 0 10px 10px;
+		}
+		</style>
+</html>
